376f533a1263a99dfcdd2a3ea198cd4352e9c765
Zhengkai Xie <xiezhengkai199982@gmail.com>
2022-11-25 00:37:12 -0500

    add signup & login front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AddSectionComponent } from './components/add_section/add-section.component';
-import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    AddSectionComponent
+    AddSectionComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +35,11 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     NgbNavModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-login-component',
@@ -7,6 +8,22 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class LoginComponent implements OnInit {
+
+    loginForm!: FormGroup;
+    constructor() { }
+
     ngOnInit(): void {
+        this.loginForm = new FormGroup(
+            {
+                email: new FormControl('', [Validators.required, Validators.email]),
+                password: new FormControl('', [Validators.required, Validators.minLength(6)])
+            }
+        );
+    }
+
+    onLogin() {
+        if (this.loginForm.valid) {
+            console.log(this.loginForm.value.email)
+        }
     }
 }
